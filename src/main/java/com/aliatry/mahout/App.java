252171375701b2227a762ab00dd24b7af825a1bd
admin-simon <admin_simon@icloud.com>
2020-03-03 23:43:52 +0800

    update

--- src/main/java/com/aliatry/mahout/App.java
+++ src/main/java/com/aliatry/mahout/App.java
@@ -22,6 +22,8 @@ import java.util.List;
 
 /**
  * Hello world!
+ *
+ * @author Simon
  */
 public class App {
     public static void main(String[] args) throws Exception {
@@ -29,8 +31,10 @@ public class App {
         // 初始化
         initial();
 
-        baseUser(); // 基于用户的协同过滤算法
-        baseItem(); // 基于物品的协同过滤算法
+        // 基于用户的协同过滤算法
+        baseUser();
+        // 基于物品的协同过滤算法
+        baseItem();
 
 
         // TODO 基于用户的协同过滤算法
@@ -50,7 +54,7 @@ public class App {
         }
     }
 
-    public static void initial() {
+    private static void initial() {
         FileOutputStream out = null;
 
         try {
@@ -88,10 +92,12 @@ public class App {
      * @throws IOException
      * @throws TasteException
      */
-    public static void baseUser() throws IOException, TasteException {
+    private static void baseUser() throws IOException, TasteException {
 
-        final int NEIGHBORHOOD_NUM = 2;   //用户邻居数量
-        final int RECOMMENDER_NUM = 3;    //推荐结果个数
+        // 用户邻居数量
+        final int NEIGHBORHOOD_NUM = 2;
+        // 推荐结果个数
+        final int RECOMMENDER_NUM = 3;
 
         /*
         准备数据 这里是电影评分数据
@@ -119,16 +125,16 @@ public class App {
         */
         Recommender r = new GenericUserBasedRecommender(dataModel, neighbor, similarity);
 
-        //得到所有用户的id集合
+        // 得到所有用户的id集合
         LongPrimitiveIterator iter = dataModel.getUserIDs();
         while (iter.hasNext()) {
             long uid = iter.nextLong();
-            //获取推荐结果，获取指定用户指定数量的推荐结果
+            // 获取推荐结果，获取指定用户指定数量的推荐结果
             List<RecommendedItem> list = r.recommend(uid, RECOMMENDER_NUM);
             System.out.printf("user : %s", uid);
-            //遍历推荐结果
+            // 遍历推荐结果
             for (RecommendedItem item : list) {
-                //获取推荐结果和推荐度
+                // 获取推荐结果和推荐度
                 System.out.print(item.getItemID() + "[" + item.getValue() + "] ");
             }
             System.out.println();
@@ -141,7 +147,7 @@ public class App {
      * @throws IOException
      * @throws TasteException
      */
-    public static void baseItem() throws IOException, TasteException {
+    private static void baseItem() throws IOException, TasteException {
         DataModel dataModel = new FileDataModel(new File("D:\\data.csv"));
         /*
         计算相似度，相似度算法有很多种，欧几里得、皮尔逊等等。
@@ -150,9 +156,9 @@ public class App {
         ItemSimilarity itemSimilarity = new PearsonCorrelationSimilarity(dataModel);
         //构建推荐器，协同过滤推荐有两种，分别是基于用户的和基于物品的，这里使用基于物品的协同过滤推荐
         GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(dataModel, itemSimilarity);
-        //给指定用户推荐若干个与指定商品相似的商品
+        // 给指定用户推荐若干个与指定商品相似的商品
         List<RecommendedItem> recommendedItemList = recommender.recommendedBecause(1, 5, 2);
-        //打印推荐的结果
+        // 打印推荐的结果
         System.out.println("根据用户1当前浏览的商品5，推荐2个相似的商品");
         for (RecommendedItem recommendedItem : recommendedItemList) {
             System.out.println(recommendedItem);
